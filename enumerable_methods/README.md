52d26785252677178e2e9d0b3cc8c82d92f6adb7
daashandayo <dj30c.1@gmail.com>
2019-04-05 16:34:41 +0530

    Add enumerable_methods

--- enumerable_methods/README.md
+++ enumerable_methods/README.md
@@ -1,3 +1,22 @@
 # Enumerable Methods
 
 Rebuiling iterator methods in `Enumerable` module to undertand how they get mixed into the Array and Hash classes (among others).
+
+## Instructions
+
+1. Open [`irb`](https://github.com/ruby/irb#usage) in your terminal.
+2. Add `enumerable_methods` methods to `irb` environment :
+    ```ruby
+    require './lib/enumerable_methods.rb'
+    ```
+
+## Usage
+
+1. Run the methods on arrays the same way their original counterparts are run
+    
+    Example:
+    ```ruby
+    # my_each : each
+    [1,2,3,4,5].my_each { |x| puts x}
+    ```
+
---
+++ enumerable_methods/lib/enumerable_methods.rb
@@ -0,0 +1,61 @@
+module Enumerable
+  
+  def my_each
+    for i in self
+      yield(i)
+    end
+    self
+  end
+
+  def my_each_with_index
+    (0..self.length-1).my_each do |x|
+      yield(self[x],x)
+    end
+    self
+  end
+
+  def my_select
+    arr = []
+    self.my_each do |x|
+      arr << x if yield(x)
+    end
+    arr
+  end
+
+  def my_all?
+    status = true
+    self.my_each do |x|
+      if !yield(x)
+        return !status
+      end
+    end
+    status
+  end
+
+  def my_any?
+    status = false
+    self.my_each do |x|
+      if yield(x)
+        return !status
+      end
+    end
+    status
+  end
+
+  def my_none?
+    status = true
+    self.my_each do |x|
+      if yield(x)
+        return !status
+      end
+    end
+    status
+  end
+
+  def my_count
+    n = 0
+    self.my_each {|x| n+=1}
+    n
+  end
+
+end
